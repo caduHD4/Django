f2d487f3f91c07ea8727ee223f5f4ce41dd08ca4
caduHD4 <cadumarianogarciapereira@gmail.com>
2024-07-08 15:58:48 -0300

    Inserção pelo template forms

--- cadastros/urls.py
+++ cadastros/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-# Importar as views
+from .views import CidadeCreate, PessoaCreate, CarroCreate, MoradorCreate, PorteiroCreate, VisitaCreate, ApartamentoCreate
 
 urlpatterns = [
-    # Criar urls para as views
+    path('cadastrar/cidade/', CidadeCreate.as_view(), name='add-cidade'),
+    path('cadastrar/pessoa/', PessoaCreate.as_view(), name='add-pessoa'),
+    path('cadastrar/carro/', CarroCreate.as_view(), name='add-carro'),
+    path('cadastrar/moradore/', MoradorCreate.as_view(), name='add-morador'),
+    path('cadastrar/porteiro/', PorteiroCreate.as_view(), name='add-porteiro'),
+    path('cadastrar/visita/', VisitaCreate.as_view(), name='add-visita'),
+    path('cadastrar/apartamento/', ApartamentoCreate.as_view(), name='add-apartamento'),
 
 ]
--- cadastros/views.py
+++ cadastros/views.py
@@ -1,4 +1,45 @@
-from django.views.generic import TemplateView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+from .models import Cidade, Pessoa, Carro, Morador, Porteiro, Visita, Apartamento
 
-class IndexView(TemplateView):
-    template_name = "cadastros/index.html"
+class CidadeCreate(CreateView):
+    model = Cidade
+    fields = ['nome', 'estado']
+    template_name = 'cadastros/form.html'
+    success_url = reverse_lazy('index')
+
+class PessoaCreate(CreateView):
+    model = Pessoa
+    fields = ['nome_completo', 'nascimento', 'cpf', 'email', 'cidade']
+    template_name = 'cadastros/form.html'
+    success_url = reverse_lazy('inicio')
+
+class CarroCreate(CreateView):
+    model = Carro
+    fields = ['placa', 'modelo', 'cor', 'apartamento']
+    template_name = 'cadastros/form.html'
+    success_url = reverse_lazy('inicio')
+
+class MoradorCreate(CreateView):
+    model = Morador
+    fields = ['nome_completo', 'nascimento', 'cpf', 'email', 'cidade', 'apartamento', 'possui_animais']
+    template_name = 'cadastros/form.html'
+    success_url = reverse_lazy('inicio')
+
+class PorteiroCreate(CreateView):
+    model = Porteiro
+    fields = ['nome', 'turno']
+    template_name = 'cadastros/form.html'
+    success_url = reverse_lazy('inicio')
+
+class VisitaCreate(CreateView):
+    model = Visita
+    fields = ['pessoa_visita', 'pessoa_visitada', 'motivo', 'data_hora_entrada', 'data_hora_saida']
+    template_name = 'cadastros/form.html'
+    success_url = reverse_lazy('inicio')
+
+class ApartamentoCreate(CreateView):
+    model = Apartamento
+    fields = ['numero', 'bloco']
+    template_name = 'cadastros/form.html'
+    success_url = reverse_lazy('inicio')
