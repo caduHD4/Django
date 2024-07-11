c5af1580999826dd0ce95009377eb384e99d2015
caduHD4 <cadumarianogarciapereira@gmail.com>
2024-07-11 13:31:29 -0300

    Todas as páginas listando, excluindo e editando

--- cadastros/views.py
+++ cadastros/views.py
@@ -7,43 +7,43 @@ class CidadeCreate(CreateView):
     model = Cidade
     fields = ['nome', 'estado']
     template_name = 'cadastros/form.html'
-    success_url = reverse_lazy('inicio')
+    success_url = reverse_lazy('list-cidade')
 
 class PessoaCreate(CreateView):
     model = Pessoa
     fields = ['nome_completo', 'nascimento', 'cpf', 'email', 'cidade']
     template_name = 'cadastros/form.html'
-    success_url = reverse_lazy('inicio')
+    success_url = reverse_lazy('list-pessoa')
 
 class CarroCreate(CreateView):
     model = Carro
     fields = ['placa', 'modelo', 'cor', 'apartamento']
     template_name = 'cadastros/form.html'
-    success_url = reverse_lazy('inicio')
+    success_url = reverse_lazy('list-carro')
 
 class MoradorCreate(CreateView):
     model = Morador
     fields = ['nome_completo', 'nascimento', 'cpf', 'email', 'cidade', 'apartamento', 'possui_animais']
     template_name = 'cadastros/form.html'
-    success_url = reverse_lazy('inicio')
+    success_url = reverse_lazy('list-morador')
 
 class PorteiroCreate(CreateView):
     model = Porteiro
     fields = ['nome', 'turno']
     template_name = 'cadastros/form.html'
-    success_url = reverse_lazy('inicio')
+    success_url = reverse_lazy('list-porteiro')
 
 class VisitaCreate(CreateView):
     model = Visita
     fields = ['pessoa_visita', 'pessoa_visitada', 'motivo', 'data_hora_entrada', 'data_hora_saida']
     template_name = 'cadastros/form.html'
-    success_url = reverse_lazy('inicio')
+    success_url = reverse_lazy('list-visita')
 
 class ApartamentoCreate(CreateView):
     model = Apartamento
     fields = ['numero', 'bloco']
     template_name = 'cadastros/form.html'
-    success_url = reverse_lazy('inicio')
+    success_url = reverse_lazy('list-apartamento')
 
 ##########UPDATE##############
 
@@ -51,80 +51,80 @@ class CidadeUpdate(UpdateView):
     model = Cidade
     fields = ['nome', 'estado']
     template_name = 'cadastros/form.html'
-    success_url = reverse_lazy('index')
+    success_url = reverse_lazy('list-cidade')
 
 class PessoaUpdate(UpdateView):
     model = Pessoa
     fields = ['nome_completo', 'nascimento', 'cpf', 'email', 'cidade']
     template_name = 'cadastros/form.html'
-    success_url = reverse_lazy('inicio')
+    success_url = reverse_lazy('list-pessoa')
 
 class CarroUpdate(UpdateView):
     model = Carro
     fields = ['placa', 'modelo', 'cor', 'apartamento']
     template_name = 'cadastros/form.html'
-    success_url = reverse_lazy('inicio')
+    success_url = reverse_lazy('list-carro')
 
 class MoradorUpdate(UpdateView):
     model = Morador
     fields = ['nome_completo', 'nascimento', 'cpf', 'email', 'cidade', 'apartamento', 'possui_animais']
     template_name = 'cadastros/form.html'
-    success_url = reverse_lazy('inicio')
+    success_url = reverse_lazy('list-morador')
 
 class PorteiroUpdate(UpdateView):
     model = Porteiro
     fields = ['nome', 'turno']
     template_name = 'cadastros/form.html'
-    success_url = reverse_lazy('inicio')
+    success_url = reverse_lazy('list-porteiro')
 
 class VisitaUpdate(UpdateView):
     model = Visita
     fields = ['pessoa_visita', 'pessoa_visitada', 'motivo', 'data_hora_entrada', 'data_hora_saida']
     template_name = 'cadastros/form.html'
-    success_url = reverse_lazy('inicio')
+    success_url = reverse_lazy('list-visita')
 
 class ApartamentoUpdate(UpdateView):
     model = Apartamento
     fields = ['numero', 'bloco']
     template_name = 'cadastros/form.html'
-    success_url = reverse_lazy('inicio')
+    success_url = reverse_lazy('list-apartamento')
 
-######DELETE######
+#######DELETE#######
 
 class CidadeDelete(DeleteView):
     model = Cidade
     template_name = 'cadastros/confirm_delete.html'
-    success_url = reverse_lazy('inicio')
+    success_url = reverse_lazy('list-cidade')
 
 class PessoaDelete(DeleteView):
     model = Pessoa
     template_name = 'cadastros/confirm_delete.html'
-    success_url = reverse_lazy('inicio')
+    success_url = reverse_lazy('list-pessoa')
 
 class CarroDelete(DeleteView):
     model = Carro
     template_name = 'cadastros/confirm_delete.html'
-    success_url = reverse_lazy('inicio')
+    success_url = reverse_lazy('list-carro')
 
 class MoradorDelete(DeleteView):
     model = Morador
     template_name = 'cadastros/confirm_delete.html'
-    success_url = reverse_lazy('inicio')
+    success_url = reverse_lazy('list-morador')
 
 class PorteiroDelete(DeleteView):
     model = Porteiro
     template_name = 'cadastros/confirm_delete.html'
-    success_url = reverse_lazy('inicio')
+    success_url = reverse_lazy('list-porteiro')
 
 class VisitaDelete(DeleteView):
     model = Visita
     template_name = 'cadastros/confirm_delete.html'
-    success_url = reverse_lazy('inicio')
+    success_url = reverse_lazy('list-visita')
 
 class ApartamentoDelete(DeleteView):
     model = Apartamento
     template_name = 'cadastros/confirm_delete.html'
-    success_url = reverse_lazy('inicio')
+    success_url = reverse_lazy('list-apartamento')
 
 #######LISTA#######
 
