1d68adb3673172c189b799fcc06db6ae85bb49ff
caduHD4 <cadumarianogarciapereira@gmail.com>
2024-09-30 20:53:53 -0300

    Add table e dados do usuario

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from django.urls import reverse_lazy
-from .views import UsuarioCreate
+from .views import UsuarioCreate, PerfilUpdate
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(
@@ -16,6 +16,7 @@ urlpatterns = [
     success_url=reverse_lazy('inicio')
 ), name="alterar-senha"),
     
-        path('registrar/', UsuarioCreate.as_view(), name='registrar'),
+    path('registrar/', UsuarioCreate.as_view(), name='registrar'),
+    path('atualizar-dados/', PerfilUpdate.as_view(), name='atualizar-dados'),
 
 ]
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,8 +1,10 @@
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from django.contrib.auth.models import User, Group
 from .forms import UsuarioForm
 from django.urls import reverse_lazy
 from django.shortcuts import get_object_or_404
+
+from .models import Perfil
 
 class UsuarioCreate(CreateView):
     template_name = "paginas/register.html"
@@ -15,6 +17,8 @@ class UsuarioCreate(CreateView):
         self.object.groups.add(grupo)
         self.object.save()
 
+        Perfil.objects.create(usuario=self.object)
+
         return url
 
     def get_context_data(self, *args, **kwargs):
@@ -23,3 +27,20 @@ class UsuarioCreate(CreateView):
         context['botao'] = "Cadastrar"
 
         return context
+    
+class PerfilUpdate(UpdateView):
+        template_name = "paginas/register.html"
+        model = Perfil
+        fields = ['nome_completo', 'cpf', 'telefone']
+        success_url = reverse_lazy('index')
+
+        def get_object(self, queryset=None):
+            self.object = get_object_or_404(Perfil, usuario=self.request.user)
+            return self.object
+        
+        def get_context_data(self, *args, **kwargs):
+            context = super().get_context_data(*args, **kwargs)
+            context['titulo'] = "Meus dados pessoais"
+            context['botao'] = "Atualizar"
+
+            return context
